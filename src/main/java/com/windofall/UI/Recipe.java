e827406ab0a46c14c3969a9c5b8dc54d5c1385b8
Prly_zero <prly.zero@gmail.com>
2023-05-30 21:16:52 +0800

    UPDATE

--- src/main/java/com/windofall/UI/Recipe.java
+++ src/main/java/com/windofall/UI/Recipe.java
@@ -2,8 +2,9 @@ package com.windofall.UI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 public class Recipe extends JFrame {
     public Recipe(){
         this.setTitle("合成表添加");
@@ -18,7 +19,7 @@ public class Recipe extends JFrame {
                 jb[i*3+j]=new JButton();
                 jb[i*3+j].setBounds(a+68,b+68,64,64);
                 a+=68;
-                jb[i*3+j].addActionListener(new Se());
+                jb[i*3+j].addMouseListener(new Se());
                 jp.add(jb[i*3+j]);
             }
             a=0;
@@ -26,20 +27,54 @@ public class Recipe extends JFrame {
         }
         JButton res = new JButton();
         res.setBounds(a+68*5,b-68,64,64);
-        res.addActionListener(new Se());
+        res.addMouseListener(new Se());
         jp.add(res);
         this.add(jp);
         this.setVisible(true);
     }
-    class Se implements ActionListener{
+    class Se implements MouseListener {
+        static Icon se_ic;
         @Override
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser jf = new JFileChooser();
-            jf.showOpenDialog(getParent());
+        public void mouseClicked(MouseEvent e) {
             JButton jb = (JButton) e.getSource();
-            if(jf.getSelectedFile()!=null){
-            jb.setIcon(new ImageIcon(new ImageIcon(jf.getSelectedFile().getPath()).getImage().getScaledInstance(64, 64, Image.SCALE_DEFAULT)));
+            if(e.getButton()==MouseEvent.BUTTON1){
+                if(e.getClickCount()==1) {
+                    JFileChooser jf = new JFileChooser();
+                    jf.showOpenDialog(getParent());
+                    if (jf.getSelectedFile() != null) {
+                        jb.setIcon(new ImageIcon(new ImageIcon(jf.getSelectedFile().getPath()).getImage().getScaledInstance(64, 64, Image.SCALE_DEFAULT)));
+                    }
+                } else if (e.getClickCount()==2) {
+                    jb.setIcon(null);
+                }
+            } else if (e.getButton()==MouseEvent.BUTTON3) {
+                if(e.getClickCount()==1){
+                    jb.setIcon(se_ic);
+                } else if (e.getClickCount()==2) {
+                    se_ic=jb.getIcon();
+                }
             }
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
         }
     }
 }
